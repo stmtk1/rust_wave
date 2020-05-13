389729eeab2aeb349cbe3aa646b9a8755868ddb5
斎藤基樹 <stmtk13044032@gmail.com>
2020-05-13 22:17:18 +0900

    アニメーションつけた

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ const WIDTH: f64 = 640.0;
 const HEIGHT: f64 = 480.0;
 const AMPLITUDE: f64 = 100.0;
 const FREQUENCY: f64 = 2.0;
+const VELOCITY: f64 = 0.01;
 
 const V_CENTER: f64 = HEIGHT / 2.0;
 
@@ -31,12 +32,14 @@ fn main() {
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
             app.render(&args);
+            app.update();
         }
     }
 }
 
 struct App {
     gl: GlGraphics,
+    t: f64,
 }
 
 
@@ -44,24 +47,30 @@ impl App {
     fn new(opengl: shader_version::opengl::OpenGL) -> App {
         App {
             gl: GlGraphics::new(opengl),
+            t: 0.0,
         }
     }
     fn render(&mut self, args: &RenderArgs) {
+        let t = self.t;
         self.gl.draw(args.viewport(), |c, gl| {
             graphics::clear(WHITE, gl);
             for i in 0..SPLIT_NUM {
-                line(BLACK, 1.0, water_wave(i), c.transform, gl);
+                line(BLACK, 1.0, water_wave(i, t), c.transform, gl);
             }
         });
     }
+
+    fn update(&mut self) {
+        self.t += VELOCITY;
+    }
 }
 
-fn water_wave(i: usize) -> [f64; 4] {
+fn water_wave(i: usize, t: f64) -> [f64; 4] {
     let x1 = (i as f64) / (SPLIT_NUM as f64) * WIDTH;
     let x2 = (i as f64 + 1.0) / (SPLIT_NUM as f64) * WIDTH;
-    [x1, V_CENTER + wave_func(i), x2, V_CENTER + wave_func(i + 1)]
+    [x1, V_CENTER + wave_func(i, t), x2, V_CENTER + wave_func(i + 1, t)]
 }
 
-fn wave_func(i: usize) -> f64 {
-    ((i as f64) * PI * 2.0 / (SPLIT_NUM as f64) * FREQUENCY).sin() * AMPLITUDE
+fn wave_func(i: usize, t: f64) -> f64 {
+    ((i as f64) * PI * 2.0 / (SPLIT_NUM as f64) * FREQUENCY + t).sin() * AMPLITUDE
 }
